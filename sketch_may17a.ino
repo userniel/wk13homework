617a2cce148f0e67b4ffb4cc8241ae1ed2a26663
userniel <dany19971027@gmail.com>
2017-05-17 11:55:25 +0800

    Add files via upload

---
+++ sketch_may17a.ino
@@ -0,0 +1,28 @@
+const int analogPin =A0;
+const int ledCount = 10;
+int ledPins[] = {2,3,4,5,6,7,8,9,10,11};
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int thisLed = 0; thisLed < ledCount; thisLed++){
+    pinMode(ledPins[thisLed], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int f = 500;
+  int sensorReading = analogRead(analogPin);
+  int ledLevel = map(sensorReading, 0, 1023, 0, ledCount);
+  for(int thisLed = ledLevel; thisLed < 10-ledLevel; thisLed++){
+    digitalWrite(ledPins[thisLed], HIGH);
+    delay(15);
+    Serial.println(ledLevel);
+    delay(1);
+  }
+  for(int thisLed = ledLevel; thisLed < 10-ledLevel; thisLed++){
+    digitalWrite(ledPins[thisLed], LOW);
+    delay(15);
+  }
+}
